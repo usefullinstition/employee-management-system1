b0f803d4e16260fffb875437f52f57972042a4cb
Hayat <awelhayat875@gmail.com>
2026-07-31 02:07:10 -0700

    add vercel url to cors

--- server/index.js
+++ server/index.js
@@ -18,7 +18,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://employee-management-system1-theta.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
