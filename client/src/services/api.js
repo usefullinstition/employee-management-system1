6031b25496339b11a272fb02318641b5c472abf2
Hayat <awelhayat875@gmail.com>
2026-09-03 00:55:47 -0700

    Configure frontend API for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,31 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
 API.interceptors.request.use(
