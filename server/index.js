885d8769b310aebec80f210e8cd269e3f205a539
Hayat <awelhayat875@gmail.com>
2026-09-03 00:15:40 -0700

    Prepare backend for Render

--- server/index.js
+++ server/index.js
@@ -76,5 +76,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
